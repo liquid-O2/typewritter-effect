2217ac72f161aad23393e491072d9a8d3413a085
arunava(liquido2) <this.is.arunava.b@gmail.com>
2023-04-07 10:35:05 +0530

    Change to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,16 +5,43 @@ import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => {
+  // -- Using just CSS
+  // useEffect(() => {
+  //   const elements = document.querySelectorAll(".marker")
+  //   elements.forEach(element => {
+  //     element.addEventListener("sal:in", ({ detail }) => {
+  //       detail.target.classList.add("typewriter")
+  //       detail.target.classList.add("typewriter-start")
+  //     })
+  //     element.addEventListener("sal:out", ({ detail }) => {
+  //       detail.target.classList.remove("typewriter")
+  //       detail.target.classList.remove("typewriter-start")
+  //     })
+  //   })
+  // }, [])
+
+  // -- Using JS
   useEffect(() => {
     const elements = document.querySelectorAll(".marker")
     elements.forEach(element => {
       element.addEventListener("sal:in", ({ detail }) => {
-        detail.target.classList.add("typewriter")
-        detail.target.classList.add("typewriter-start")
+        const text = element.innerHTML
+        element.innerHTML = ""
+        let index = 0
+        detail.target.classList.remove("end")
+        detail.target.classList.add("start")
+        function typeWriter() {
+          if (index < text.length) {
+            detail.target.innerHTML += text.charAt(index)
+            index++
+            setTimeout(typeWriter, 100)
+          }
+        }
+        typeWriter()
       })
       element.addEventListener("sal:out", ({ detail }) => {
-        detail.target.classList.remove("typewriter")
-        detail.target.classList.remove("typewriter-start")
+        detail.target.classList.remove("start")
+        detail.target.classList.add("end")
       })
     })
   }, [])
@@ -27,15 +54,19 @@ const IndexPage = () => {
       <section className='card-wrapper'>
         <figure>
           <StaticImage src='../images/imageOne.jpg' alt='Image One' quality={80} placeholder='blurred' />
-          <p className='typewriter marker' data-sal>
-            Casa Blanca
-          </p>
+          <figcaption className='typewriter-wrapper'>
+            <p className='typewriter marker' data-sal>
+              Casa Blanca
+            </p>
+          </figcaption>
         </figure>
         <figure>
           <StaticImage src='../images/imageTwo.jpg' alt='Image Two' quality={80} placeholder='blurred' />
-          <p className='typewriter marker' data-sal>
-            Scandanavian Delight
-          </p>
+          <figcaption className='typewriter-wrapper'>
+            <p className='typewriter marker' data-sal>
+              Scandanavian Delight
+            </p>
+          </figcaption>
         </figure>
       </section>
     </Layout>
