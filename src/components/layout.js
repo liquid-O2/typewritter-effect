1aaaa9b0ba4b894687385cd276a73674d3ce16e2
arunava(liquido2) <this.is.arunava.b@gmail.com>
2023-04-10 09:49:41 +0530

    Change to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,23 +1,35 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
+import { navigate } from "gatsby"
+import { useLocation } from "@reach/router"
 import "../sass/site.scss"
 import Nav from "./nav"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const { pathname } = useLocation()
+
+  React.useEffect(() => {
+    const curtain = document.querySelector(".curtain")
+
+    curtain.classList.add("fade-in")
+    const links = document.querySelectorAll("a")
+    links.forEach(link => {
+      link.getAttribute("target") === null &&
+        link.addEventListener("click", function fadeAnimation(e) {
+          e.preventDefault()
+          const url = link.getAttribute("href")
+          url !== pathname && curtain.classList.add("fade-out")
+          setTimeout(() => {
+            navigate(url)
+          }, 700)
+        })
+    })
+  }, [pathname])
 
   return (
     <>
-      <Nav/>
-      <main className="h-100vh">{children}</main>
+      <Nav />
+      <main className='h-100vh curtain'>{children}</main>
     </>
   )
 }
